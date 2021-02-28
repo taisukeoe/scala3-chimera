444a967638911e5dc13879beca5469bcb69dad72
Taisuke Oe <oeuia.t@gmail.com>
2021-02-28 21:58:29 +0900

    rewrite Json.format to Format.of with Scala 3

--- domain/src/main/scala/User.scala
+++ domain/src/main/scala/User.scala
@@ -1,17 +1,22 @@
 package domain
 
-
-// case class Name(value: String) extends AnyVal
-
 object User {
+  // case class Id(val value: Int) extends AnyVal
+  // case class Name(val value: String) extends AnyVal
+
   opaque type Id = Int
   opaque type Name = String
   object Id {
       def apply(i: Int): Id = i
   }
+  extension (id: Id)
+    def value: Int = id
+  
   object Name {
       def apply(n: String): Name = n
   }
+  extension (name: Name)
+    def value: String = name
 }
 
 case class User(id: User.Id, name: User.Name)
